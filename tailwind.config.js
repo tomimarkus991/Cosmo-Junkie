9d61493ce145951010ca65d28a5b94238b23cee4
Tomi Markus Alber <tomimarkus.alber@tptlive.ee>
2020-07-22 21:23:40 +0300

    Added and finished the project

---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        yellow: '#FFC636',
+        blue: '#0A0B5B',
+        darkBlue: '#07083E',
+        darkerBlue: '#080936',
+        backgroundFooter: '#14153D',
+        purply: '#9893D8',
+        purply2: '#9994F7',
+        purplyWhite: '#f2f2f2',
+        gray: '#222',
+        h: '#DDDBFF'
+      },
+      fontFamily: {
+        rubikRegular: ["Rubik", "sans-serif"]
+      },
+      width: {
+        '10': '10%',
+        '20': '20%',
+        '30': '30%',
+        '80': '80%',
+        '200': '200%',
+        '400': '400%',
+      },
+      maxWidth: {
+        '30': '30%',
+        '40': '40%',
+        '50': '50%',
+        '58': '58%',
+        '60': '60%',
+        '65': '65%',
+        '70': '70%',
+        '80': '80%',
+        '90': '90%',
+        '100': '100%',
+        '110': '110%',
+        '120': '120%',
+        '130': '130%',
+        '180': '180%',
+        '200': '200%',
+        '400': '400px',
+        '1060': '1060px'
+      },
+      backgroundSize: {
+        '40': '40%'
+      },
+      screens: {
+        'tablet': '555px',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
